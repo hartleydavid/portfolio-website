bae110e9276f5712d00e99c99b0fadc5b6f1d065
hartleydavid <56169730+hartleydavid@users.noreply.github.com>
2025-02-27 07:46:24 -0800

    Removed old code that was commented out.

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -42,20 +42,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-/*export default function Navbar() {
-
-  return (
-    <nav className="p-4 bg-gray-800 text-white flex justify-between">
-      <h1 className="text-xl font-bold">My Portfolio</h1>
-      <div>
-        <Link href="/">Home</Link>
-        <Link href="/about" className="ml-4">About</Link>
-        <Link href="/projects" className="ml-4">Projects</Link>
-        <Link href="/work" className="ml-4">Work Experience</Link>
-        <Link href="/contact" className="ml-4">Contact</Link>
-
-      </div>
-    </nav>
-  );
-
-}*/
