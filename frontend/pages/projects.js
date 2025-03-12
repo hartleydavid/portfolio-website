447d35d78dafdff554febb8c3a46c521abc32357
hartleydavid <56169730+hartleydavid@users.noreply.github.com>
2025-03-11 21:36:37 -0700

    More API env updates

--- frontend/pages/projects.js
+++ frontend/pages/projects.js
@@ -7,13 +7,9 @@ export default function Projects() {
 	const [filteredProjects, setFilteredProjects] = useState([]);
 	const [statusFilter, setStatusFilter] = useState("");
 	const [selectedTechnologies, setSelectedTechnologies] = useState([]);
-	//const API_KEY = process.env.API_KEY
 
-	console.log("API Key  Out:", process.env.API_KEY);
 	//Use effect to fetch the projects from our graphQL API (POST as we are making a query to GraphQL)
 	useEffect(() => {
-		
-		console.log("API Key:", process.env.API_KEY);  // Log to check the value
 
 		fetch("https://portfolio-website-ot1g.onrender.com/graphql", {
 			method: "POST",
