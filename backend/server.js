db178a6b07f0d4f0f8b81903448ccc9e9173c9b8
hartleydavid <56169730+hartleydavid@users.noreply.github.com>
2025-03-11 20:09:14 -0700

    Small updates to fix deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
